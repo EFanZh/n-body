f73f236c9c2e8122fa27b101effb01e8dd951143
EFanZh <efanzh@gmail.com>
2019-04-06 18:03:36 +0800

    Fix typo

--- tools/generate-readme.sh
+++ tools/generate-readme.sh
@@ -4,4 +4,4 @@ COMMIT=$(git rev-parse HEAD)
 
 echo "# N-Body
 
-Automacally generated from commit [$COMMIT](https://github.com/EFanZh/n-body/tree/$COMMIT)."
+Automatically generated from commit [$COMMIT](https://github.com/EFanZh/n-body/tree/$COMMIT)."
