33a6f43f5968af7ae47433e09b2a420998b1fbf0
EFanZh <efanzh@gmail.com>
2019-04-06 20:18:52 +0800

    Add commit message to generated README.md

--- tools/generate-readme.sh
+++ tools/generate-readme.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 COMMIT=$(git rev-parse HEAD)
+COMMIT_MESSAGE=$(git show -s --format=%B "$COMMIT")
 
 echo "# N-Body
 
-Automatically generated from commit [$COMMIT](https://github.com/EFanZh/n-body/tree/$COMMIT)."
+Automatically generated from commit [$COMMIT](https://github.com/EFanZh/n-body/tree/$COMMIT)
+([diff](https://github.com/EFanZh/n-body/commit/$COMMIT)).
+
+Commit message:
+
+"'```'"
+$COMMIT_MESSAGE
+"'```'
