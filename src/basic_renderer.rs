18bfdc4edd769ceba2cbb843f670605a5a4b17c0
EFanZh <efanzh@gmail.com>
2019-04-06 19:18:10 +0800

    Reduce the number of context switches

--- src/basic_renderer.rs
+++ src/basic_renderer.rs
@@ -10,11 +10,11 @@ pub struct BasicRenderer {
     body_colors: Vec<String>,
     trail_widths: Vec<f64>,
     sample_frequency: f64,
-    step_size: f64,
-    sampled: u64,                           // State.
-    last_body_positions: Vec<Vector2<f64>>, // State.
-    last_timestamp: f64,                    // State.
-    start_time: f64,                        // State.
+    step_size: f64,                             // Pre-calculated.
+    sampled: u64,                               // State.
+    last_timestamp: f64,                        // State.
+    start_time: f64,                            // State.
+    position_histories: Vec<Vec<Vector2<f64>>>, // Shared Buffer.
 }
 
 impl BasicRenderer {
@@ -30,9 +30,9 @@ impl BasicRenderer {
             sample_frequency: sample_frequency / 1000.0,
             step_size: sample_frequency.recip(),
             sampled: 0,
-            last_body_positions: universe.get_bodies().iter().map(|b| b.position).collect(),
             last_timestamp: 0.0,
             start_time: 0.0,
+            position_histories: universe.get_bodies().iter().map(|b| vec![b.position]).collect(),
         }
     }
 }
@@ -58,31 +58,54 @@ impl Renderer for BasicRenderer {
             timestamp = self.last_timestamp + 1000.0;
         }
 
-        // Do drawing.
+        // Collect the required drawing actions.
 
         let target_samples = (self.sample_frequency * timestamp) as _;
 
         for _ in self.sampled..target_samples {
             universe.advance(self.step_size);
 
-            for (last_position, position, color, trail_width) in izip!(
-                &mut self.last_body_positions,
-                universe.get_bodies().iter().map(|b| b.position),
-                &self.body_colors,
-                &self.trail_widths
-            ) {
-                if (position - *last_position).magnitude2() >= 1.0 {
-                    context.set_stroke_style(&JsValue::from_str(&color));
-                    context.set_line_width(*trail_width);
-
-                    context.begin_path();
-                    context.move_to(last_position.x, last_position.y);
-                    context.line_to(position.x, position.y);
-                    context.stroke();
-
-                    *last_position = position;
+            for (position_history, position) in self
+                .position_histories
+                .iter_mut()
+                .zip(universe.get_bodies().iter().map(|b| b.position))
+            {
+                if (position - position_history.last().unwrap()).magnitude2() >= 1.0 {
+                    position_history.push(position);
                 }
             }
+        }
+
+        // Do actual drawings.
+
+        for (position_history, color, trail_width) in
+            izip!(&self.position_histories, &self.body_colors, &self.trail_widths)
+        {
+            if position_history.len() > 1 {
+                context.set_stroke_style(&JsValue::from_str(&color));
+                context.set_line_width(*trail_width);
+
+                context.begin_path();
+
+                let (first_position, rest_positions) = position_history.split_first().unwrap();
+
+                context.move_to(first_position.x, first_position.y);
+
+                for position in rest_positions {
+                    context.line_to(position.x, position.y);
+                }
+
+                context.stroke();
+            }
+        }
+
+        // Discard old histories.
+
+        for position_history in &mut self.position_histories {
+            let last_index = position_history.len() - 1;
+
+            position_history.swap(0, last_index);
+            position_history.truncate(1);
         }
 
         self.sampled = target_samples;
